d2aa81f25b39a8f8bbc5254db6ad0b571f70ccba
Jorge Guberte <jorgeguberte@gmail.com>
2022-12-28 09:29:41 -0300

    Added sorting to TrackList component

--- src/components/Dashboard/TrackList.vue
+++ src/components/Dashboard/TrackList.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref } from "vue";
 import TrackComponent from "./TrackComponent.vue";
 import { usePlaylistStore } from "../../stores/PlaylistsStore";
 import { onBeforeMount } from "vue";
@@ -8,34 +9,55 @@ import { classifyTrack } from "../../use/useTrackClassifier";
 const props = defineProps(["tracks"]);
 const playlistStore = usePlaylistStore();
 
+let tracksClassified = ref(false);
+
 defineEmits(["sortPlaylist","parentFunction"]);
 
 onBeforeMount(async () => {
   for (const track of props.tracks) {
-    //console.log(track.added_at)
     const trackClassification = await classifyTrack(track.track.analysis);
     track.track.analysis.mood = trackClassification[0].label;
   }
+
+  tracksClassified.value = true;
+  
 });
 
+const playlist_sorting = ref({by:'default', order: true});
+
 function sortPlaylist(by){
-  if(by == 'default'){
-    props.tracks.sort((a,b) => (a.track.added_at > b.track.added_at) ? 1 : ((b.track.added_at > a.track.added_at) ? -1 : 0))
+  
+  if(by == playlist_sorting.by){
+    props.tracks.reverse();
   }else{
-    props.tracks.sort((a,b) => (a.track.analysis['key'] > b.track.analysis['key']) ? 1 : ((b.track.analysis['key'] > a.track.analysis['key']) ? -1 : 0))
+    if(by == 'default' || by == 'date_added'){
+      props.tracks.sort((a,b) => (a.track.added_at > b.track.added_at) ? 1 : ((b.track.added_at > a.track.added_at) ? -1 : 0))
+    }else{
+      props.tracks.sort((a,b) => (a.track.analysis[by] > b.track.analysis[by]) ? 1 : ((b.track.analysis[by] > a.track.analysis[by]) ? -1 : 0))
+    }
   }
-  //props.tracks.sort((a,b) => (a.track.analysis[by] > b.track.analysis[by]) ? 1 : ((b.track.analysis[by] > a.track.analysis[by]) ? -1 : 0))
+
+
+  playlist_sorting.by = by;
+  
+
+  playlist_sorting.order = !playlist_sorting.order;
+
+  
+  
 }
+
+
 </script>
 
 <template>
-  <button @click="sortPlaylist('key')">Sort</button>
+  <button @click="sortPlaylist('default')">Sort</button>
   <ul class="mb-5 pt-5">
     <div class="flex bg-red-200 flex-col ml-3 mr-3 items-end">
       <div class="w-1/5 bg-blue-200 flex flex-row text-center">
-        <p class="w-1/3">Key<i class="fa-solid fa-sort pl-1 text-xs text-neutral-600 cursor-pointer" @click="$emit('sortPlaylist', 'key')"></i></p>
-        <p class="w-1/3">Tempo<i class="fa-solid fa-sort pl-1 text-xs text-neutral-600 cursor-pointer" @click="$emit('sortPlaylist', 'tempo')"></i></p>
-        <p class="w-1/3">Mood<i class="fa-solid fa-sort pl-1 text-xs text-neutral-600 cursor-pointer" @click="$emit('sortPlaylist', 'mood')"></i></p>
+        <p class="w-1/3">Key<i class="fa-solid fa-sort pl-1 text-xs text-neutral-600 cursor-pointer" @click="sortPlaylist('key')"></i></p>
+        <p class="w-1/3">Tempo<i class="fa-solid fa-sort pl-1 text-xs text-neutral-600 cursor-pointer" @click="sortPlaylist('tempo')"></i></p>
+        <p class="w-1/3">Mood<i class="fa-solid fa-sort pl-1 text-xs text-neutral-600 cursor-pointer" @click="sortPlaylist('mood')"></i></p>
       </div>
     </div>
     <TrackComponent v-for="track in props.tracks" :key="track.id" :track="track" />
