1bdd56810660ebde0c3884b89b663f07ac15b686
Jorge Guberte <jorgeguberte@gmail.com>
2023-01-30 09:21:42 -0300

    content scrolling on sidebar

--- src/views/NewDashboard.vue
+++ src/views/NewDashboard.vue
@@ -2,9 +2,14 @@
     <div class="dashboard">
         <div class="sidebar">
 
-            <h1>Sidebar</h1>
             <div class="sidebar-lid" v-if="!isMobile">
-                lid
+                <h1>Maestro</h1>
+                <p>Choose a playlist below</p>
+            </div>
+            <div class="sidebar-playlist-container">
+                <ul class="h-full">
+                    <li v-for="i in 100">sasa</li>
+                </ul>
             </div>
         </div>
         <div class="content"><h1>Content</h1></div>
@@ -14,21 +19,26 @@
 <script setup>
 import {breakpointsTailwind, useBreakpoints} from '@vueuse/core';
 
+//Check for mobile
 const breakpoints = useBreakpoints(breakpointsTailwind);
-
 const isMobile = breakpoints.smallerOrEqual('md');
+
+
 </script>
 
 <style scoped>
 .dashboard{
-    @apply bg-neutral-200 md:bg-emerald-200 h-screen;
+    @apply bg-neutral-200 md:bg-emerald-200 h-screen overflow-y-scroll;
     @apply flex flex-col md:flex-row;
 }
 .sidebar{
-    @apply w-full md:w-1/3 lg:w-1/4 h-1/6 md:h-full bg-red-200
+    @apply w-full md:w-1/3 lg:w-1/4 h-1/6 md:h-screen overflow-hidden bg-red-200
 }
 
 .sidebar-lid{
-    @apply bg-blue-200
+    @apply bg-blue-200 p-3
+}
+.sidebar-playlist-container{
+    @apply h-full overflow-y-scroll
 }
 </style>
