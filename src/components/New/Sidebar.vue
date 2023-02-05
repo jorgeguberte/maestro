5dc419cc10114f33e3412608dfe9b771c1daac07
Jorge Guberte <jorgeguberte@gmail.com>
2023-02-04 22:13:50 -0300

    large size stable

---
+++ src/components/New/Sidebar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex flex-col md:w-1/3 lg:w-1/4">
+    <!--cover-->
+    <div class="sidebar-cover md:h-full p-3 flex bg-red-200">
+      <h2>Hello! I'm Maestro</h2>
+    </div>
+    <!--items-->
+    <div class="sidebar-items overflow-y-scroll">
+      <ul>
+        <li v-for="x in 100">sidebar: {{ x }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
--- src/views/NewDashboard.vue
+++ src/views/NewDashboard.vue
@@ -1,58 +1,39 @@
 <template>
-    <div class="dashboard">
-        <div class="sidebar">
-
-            <div class="sidebar-lid" v-if="!isMobile">
-                <h1>Maestro</h1>
-                <p>Choose a playlist below</p>
-            </div>
-            <div class="sidebar-playlist-container">
-                <div class="h-fit">
-                    <!--<ul class="h-full flex overflow-x-clip md:flex-col">
-                    <li v-for="i in 50">
-                        <div>{{i}}</div>
-                    </li>
-                </ul>-->
-                <ul>
-                    <li v-for="x in 100">{{ x }}</li>
-                </ul>
-                </div>
-            </div>
-        </div>
-        <div class="content">
-            <h1>Content</h1>
-            <p v-for="x in 200">{{x}}</p>
-        </div>
+  <div class="dashboard">
+    <Sidebar />
+    <div class="content">
+      <h1>Content</h1>
+      <p v-for="x in 200">content: {{ x }}</p>
     </div>
+  </div>
 </template>
 
 <script setup>
-import {breakpointsTailwind, useBreakpoints} from '@vueuse/core';
+import { breakpointsTailwind, useBreakpoints } from "@vueuse/core";
+import Sidebar from "../components/New/Sidebar.vue";
 
 //Check for mobile
 const breakpoints = useBreakpoints(breakpointsTailwind);
-const isMobile = breakpoints.smallerOrEqual('md');
-
-
+const isMobile = breakpoints.smallerOrEqual("md");
 </script>
 
 <style scoped>
-.dashboard{
-    @apply bg-neutral-200 md:bg-emerald-200 h-screen;
-    @apply flex flex-col md:flex-row;
+.dashboard {
+  @apply bg-neutral-200 md:bg-emerald-200 h-screen;
+  @apply flex flex-col md:flex-row;
 }
-.sidebar{
-    @apply  md:w-1/3 lg:w-1/4 h-1/6 md:h-screen overflow-hidden bg-red-200;
-}
-
-.sidebar-lid{
-    @apply bg-blue-200 p-3 ;
-}
-.sidebar-playlist-container{
-    @apply h-full overflow-y-scroll
+.sidebar {
+  @apply md:w-1/3 lg:w-1/4 h-1/6 md:h-full overflow-y-hidden;
 }
 
-.content{
-    @apply h-full w-full overflow-y-scroll;
+.sidebar-lid {
+  @apply p-3;
+}
+.sidebar-playlist-container {
+  @apply h-full overflow-y-scroll;
+}
+
+.content {
+  @apply h-full w-full overflow-y-scroll;
 }
 </style>
