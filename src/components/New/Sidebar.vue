2a862adc519cd67d77c5d34b3f17690373e005bc
Jorge Guberte <jorgeguberte@gmail.com>
2023-02-05 07:28:38 -0300

    Adaptive layout working as expected

--- src/components/New/Sidebar.vue
+++ src/components/New/Sidebar.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="flex flex-col md:w-1/3 lg:w-1/4">
+  <div class="flex flex-col sm:bg-red-200 md:w-1/3 lg:w-1/4">
     <!--cover-->
-    <div class="sidebar-cover md:h-full p-3 flex bg-red-200">
+    <div class="sidebar-cover md:h-1/6 p-3 flex bg-red-200" v-if="!isMobile">
       <h2>Hello! I'm Maestro</h2>
     </div>
     <!--items-->
-    <div class="sidebar-items overflow-y-scroll">
+    <div class="sidebar-items lg:overflow-y-scroll sm:bg-purple-500 lg:bg-teal-600">
       <ul>
         <li v-for="x in 100">sidebar: {{ x }}</li>
       </ul>
@@ -13,6 +13,11 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { breakpointsTailwind, useBreakpoints } from "@vueuse/core";
+
+const breakpoints = useBreakpoints(breakpointsTailwind);
+const isMobile = breakpoints.smallerOrEqual("md");
+</script>
 
 <style scoped></style>
--- src/views/NewDashboard.vue
+++ src/views/NewDashboard.vue
@@ -1,39 +1,27 @@
 <template>
-  <div class="dashboard">
-    <Sidebar />
-    <div class="content">
-      <h1>Content</h1>
-      <p v-for="x in 200">content: {{ x }}</p>
-    </div>
-  </div>
+<div class="wrapper"><!-- Outer wrapper-->
+    <div class="sidebar">sidebar</div><!--left column-->
+    <div class="content">content</div><!--left column-->
+</div>
 </template>
 
 <script setup>
-import { breakpointsTailwind, useBreakpoints } from "@vueuse/core";
-import Sidebar from "../components/New/Sidebar.vue";
 
-//Check for mobile
-const breakpoints = useBreakpoints(breakpointsTailwind);
-const isMobile = breakpoints.smallerOrEqual("md");
 </script>
 
 <style scoped>
-.dashboard {
-  @apply bg-neutral-200 md:bg-emerald-200 h-screen;
-  @apply flex flex-col md:flex-row;
-}
-.sidebar {
-  @apply md:w-1/3 lg:w-1/4 h-1/6 md:h-full overflow-y-hidden;
+.wrapper{
+  @apply grid grid-flow-row lg:grid-flow-col;
+  @apply w-screen h-screen;
 }
 
-.sidebar-lid {
-  @apply p-3;
+.content{
+  @apply bg-slate-400;
+  @apply row-span-6
 }
-.sidebar-playlist-container {
-  @apply h-full overflow-y-scroll;
-}
-
-.content {
-  @apply h-full w-full overflow-y-scroll;
+.sidebar{
+  @apply  bg-slate-200  ;
+  @apply order-last lg:order-first;
+  @apply h-full lg:h-screen;
 }
 </style>
