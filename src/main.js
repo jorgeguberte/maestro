3a11906e4b51cccce0aabd7163f40bb57928825e
Jorge Guberte <jorgeguberte@gmail.com>
2024-03-08 01:01:05 -0300

    Firebase integration

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue';
+import {initializeApp} from 'firebase/app';
+import {getAnalytics} from 'firebase/analytics';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 import { createPinia } from 'pinia';
 //import './style.css'
@@ -10,11 +15,25 @@ import CallbackView from './views/CallbackView.vue';
 
 
 import router from "./router";
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  };
+
+const firebaseApp = initializeApp(firebaseConfig);
+
 
 const pinia = createPinia()
 const app = createApp(App)
+const analytics = getAnalytics(firebaseApp);
 
 
+app.config.globalProperties.$analytics = analytics;
 app.use(pinia)
 app.use(router)
 app.mount('#app')
