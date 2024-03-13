0219c22c9d49acd44203178821431612af4347a4
Jorge Guberte <jorgeguberte@gmail.com>
2024-03-13 02:43:51 -0300

    Added results list

--- src/views/NewDashboard.vue
+++ src/views/NewDashboard.vue
@@ -107,20 +107,27 @@ async function search(){
 </script>
 
 <template>
-    <div id="app" class="mx-container auto w-screen h-screen bg-red-200 flex justify-center overflow-clip ">
-      <div class=""><!--#TODO: Make SearchBox component with the contents of this div-->
+    <div id="app" class="container mx-auto w-screen h-screen bg-red-200 flex flex-col">
+      <header class="sticky top-0 z-10 p-4 bg-blue-200 h-fit text-center"> <h1 class="text-2xl font-bold">Maestro</h1>
+        <p class="text-gray-600">Lorem ipsum dolor sit amet...</p> 
+    </header>
+    <div class="sticky top-16 p-4 text-center">
       <input v-model="query" type="text" placeholder="Search for a song on Spotify">
       <button @click="search">Search</button>
     </div>
-      <div id="results" v-if="results.length > 0" class="overflow-scroll max-h-screen pt-10">
-        <div v-for="track in results" :key="track.id">
-          <p> {{track.artists[0].name}} </p>
-          <p>{{ track.name }}</p>
-          <p>{{track.album.name}} </p>
-          <img  :src="track.album.images[2].url" alt="Album cover">
+    <div class=" overflow-y-auto max-h-[calc(100vh-12rem)] bg-orange-300 w-[screen]"><!-- results wrapper-->
+      <div v-for="track in results" :key="track.id" class="w-full p-2 flex items-center gap-4 border cursor-pointer hover:bg-gray-100">
+        <img  :src="track.album.images[2].url" alt="Album cover" class="w-16 h-16">
+        <div>
+          <h2>{{ track.name }}</h2>
+          <p>{{ track.artists[0].name }}</p>
         </div>
-        <!--<p v-if="!results.length">No results found.</p>--->
       </div>
+
+    </div>
+
+
+
     </div>
   </template>
   
